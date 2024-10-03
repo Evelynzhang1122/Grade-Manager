1d1396ec696564d84196e99353758635f2f76daf
zhxny <zhanhaohe1@gmail.com>
2024-10-02 21:36:25 -0700

    Add files via upload

--- group5_iteration1.cpp
+++ group5_iteration1.cpp
@@ -33,71 +33,64 @@ int main()
 // Find whether assignments are weighted
 // If all assignments hold equal weight, go by a point-based system (easier to code)
 // If assignments hold different weight, add percentages of how much does assignments, tests, projects, etc. make up the grade (up to 100%)
-
-    cout << "Are assignments weighted in this course? (yes/no): ";
-    cin >> weighted;
-    
-
-// Later in the code: user output of whether they are doing good, or need to do better in the course
-// Also add code later to see if there are any other courses to test
 // Determine how the class is graded
 // Find the percentage/points required to get each letter grade
 
-}
+    cout << "Are assignments weighted in this course? (yes/no): ";
+    cin >> weighted;
+
     if (weighted == "yes") {
 
-    cout << "Minimum Percentage required to get an A?: ";
-    cin >> GradeA;
-    cout << "Minimum Percentage required to get a B?: ";
-    cin >> GradeB;
-    cout << "Minimum Percentage required to get a C?: ";
-    cin >> GradeC;
-    cout << "Minimum Percentage required to get a D?: ";
-    cin >> GradeD;
-    cout << "How much points is the class made up of?: ";
-    cin >> TotalPoints;
+        cout << "How much points is the class made up of?: ";
+        cin >> TotalPoints;
+        cout << "Minimum Percentage required to get an A?: ";
+        cin >> GradeA;
+        cout << "Minimum Percentage required to get a B?: ";
+        cin >> GradeB;
+        cout << "Minimum Percentage required to get a C?: ";
+        cin >> GradeC;
+        cout << "Minimum Percentage required to get a D?: ";
+        cin >> GradeD;
 
-    int assignmentWeight, examWeight, projectWeight;
-    cout << "Enter the percentage weight of assignments: ";
-    cin >> assignmentWeight;
-    cout << "Enter the percentage weight of exams: ";
-    cin >> examWeight;
-    cout << "Enter the percentage weight of projects: ";
-    cin >> projectWeight;
+        int assignmentWeight, examWeight, projectWeight;
+        cout << "Enter the percentage weight of assignments: ";
+        cin >> assignmentWeight;
+        cout << "Enter the percentage weight of exams: ";
+        cin >> examWeight;
+        cout << "Enter the percentage weight of projects: ";
+        cin >> projectWeight;
 
-    // Add checks to make sure weights sum to 100%
-    if (assignmentWeight + examWeight + projectWeight != 100) {
-        cout << "The weights must add up to 100%. Please re-enter the weights.\n";
-        // Loop back to input the weights again if they don't sum to 100.
-    }
+        // Add checks to make sure weights sum to 100%
+      if (assignmentWeight + examWeight + projectWeight != 100) {
+            cout << "The weights must add up to 100%. Please re-enter the weights.\n";
+            // Loop back to input the weights again if they don't sum to 100.
+        } 
+    } else if (weighted == "no") {
 
-    else if (weighted == "no") {
+        cout << "How much points is the class made up of?: ";
+        cin >> TotalPoints;
+        cout << "Minimum Points required to get an A?: ";
+        cin >> GradeA;
+        cout << "Minimum Points required to get a B?: ";
+        cin >> GradeB;
+        cout << "Minimum Points required to get a C?: ";
+        cin >> GradeC;
+        cout << "Minimum Points required to get a D?: ";
+        cin >> GradeD;
+        
+        int assignmentPoints, examPoints, projectPoints;
+        cout << "Enter the amount of points your assignments make up in the course: ";
+        cin >> assignmentPoints;
+        cout << "Enter the amount of points your exams make up in the course: ";
+        cin >> examPoints;
+        cout << "Enter the amount of points your projects make up in the course: ";
+        cin >> projectPoints;
+        // Non-weighted system: Simple percentage-based calculation
 
-    cout << "Minimum Points required to get an A?: ";
-    cin >> GradeA;
-    cout << "Minimum Points required to get a B?: ";
-    cin >> GradeB;
-    cout << "Minimum Points required to get a C?: ";
-    cin >> GradeC;
-    cout << "Minimum Points required to get a D?: ";
-    cin >> GradeD;
-    cout << "How much points is the class made up of?: ";
-    cin >> TotalPoints;
-    
-    int assignmentPoints, examPoints, projectPoints;
-    cout << "Enter the amount of points your assignments make up in the course: ";
-    cin >> assignmentPoints;
-    cout << "Enter the amount of points your exams make up in the course: ";
-    cin >> examPoints;
-    cout << "Enter the amount of points your projects make up in the course: ";
-    cin >> projectPoints;
-    // Non-weighted system: Simple percentage-based calculation
-    
-
-}    
-    else {
+    } else {
     cout << "This question must be answered properly. Please try again.\n";
     // Loop back to input for weighted/unweighted question
-        
-
+    }
+// Later in the code: user output of whether they are doing good, or need to do better in the course
+// Also add code later to see if there are any other courses to test
 }
