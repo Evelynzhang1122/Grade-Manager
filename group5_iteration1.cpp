dab1c8fa6ff3936f4237079559524961dbec4499
zhxny <zhanhaohe1@gmail.com>
2024-12-03 18:24:21 -0800

    Add files via upload

--- group5_iteration1.cpp
+++ group5_iteration1.cpp
@@ -17,27 +17,27 @@ using namespace std;
 
 int main()
 {
-    string Name, Class, weighted;
+    string Name, Class, weightChoice, choice;
     int StudentID, TotalPoints, GradeA, GradeB, GradeC, GradeD, GradeF;
 
 // Add code in which if name isn't letters, or id isn't an integer, ask user to reinput
+    do {
+        cout << "Enter a name: ";
+        cin >> Name;
 
-    cout << "Enter a name: ";
-    cin >> Name;
+        cout << "Enter your school ID: ";
+        cin >> StudentID;
 
-    cout << "Enter your school ID: ";
-    cin >> StudentID;
-
-    if (cin.fail()) {  // will fail if the input is not an int. It will then restart by calling the main funciton
+        if (cin.fail()) {  // will fail if the input is not an int. It will then restart by calling the main funciton
         cout << "Invalid input. Please Restart" << endl;
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');  // discards the invalid input
         //Source: https://stackoverflow.com/questions/25020129/cin-ignorenumeric-limitsstreamsizemax-n
         main();
-    }
+        }
 
-    cout << "Enter a class: ";
-    cin >> Class;
+        cout << "Enter a class: ";
+        cin >> Class;
 
 // Find whether assignments are weighted
 // If all assignments hold equal weight, go by a point-based system (easier to code)
@@ -45,34 +45,21 @@ int main()
 // Determine how the class is graded
 // Find the percentage/points required to get each letter grade
 
-    cout << "Are assignments weighted in this course? (yes/no): ";
-    cin >> weighted;
+        cout << "Are assignments weighted in this course? (y/n): ";
+        cin >> weightChoice;
 
-    if (weighted == "yes") {
+        if (weightChoice == "y" || weightChoice == "Y") {
 
-        cout << "Minimum Percentage required to get an A?: ";
-        cin >> GradeA;
-        cout << "Minimum Percentage required to get a B?: ";
-        cin >> GradeB;
-        cout << "Minimum Percentage required to get a C?: ";
-        cin >> GradeC;
-        cout << "Minimum Percentage required to get a D?: ";
-        cin >> GradeD;
+            cout << "Minimum Percentage required to get an A?: ";
+            cin >> GradeA;
+            cout << "Minimum Percentage required to get a B?: ";
+            cin >> GradeB;
+            cout << "Minimum Percentage required to get a C?: ";
+            cin >> GradeC;
+            cout << "Minimum Percentage required to get a D?: ";
+            cin >> GradeD;
 
-        int assignmentWeight, quizWeight, examWeight, projectWeight;
-        cout << "Enter the percentage weight of assignments: ";
-        cin >> assignmentWeight;
-        cout << "Enter the percentage weight of quizzes: ";
-        cin >> quizWeight;
-        cout << "Enter the percentage weight of exams: ";
-        cin >> examWeight;
-        cout << "Enter the percentage weight of projects: ";
-        cin >> projectWeight;
-
-        // Add checks to make sure weights sum to 100%
-      while (assignmentWeight + quizWeight + examWeight + projectWeight != 100) {
-            cout << "The weights must add up to 100%. Please re-enter the weights.\n";
-            // Loop back to input the weights again if they don't sum to 100.
+            int assignmentWeight, quizWeight, examWeight, projectWeight;
             cout << "Enter the percentage weight of assignments: ";
             cin >> assignmentWeight;
             cout << "Enter the percentage weight of quizzes: ";
@@ -81,7 +68,20 @@ int main()
             cin >> examWeight;
             cout << "Enter the percentage weight of projects: ";
             cin >> projectWeight;
-        } 
+
+        // Add checks to make sure weights sum to 100%
+            while (assignmentWeight + quizWeight + examWeight + projectWeight != 100) {
+                cout << "The weights must add up to 100%. Please re-enter the weights.\n";
+                // Loop back to input the weights again if they don't sum to 100.
+                cout << "Enter the percentage weight of assignments: ";
+                cin >> assignmentWeight;
+                cout << "Enter the percentage weight of quizzes: ";
+                cin >> quizWeight;
+                cout << "Enter the percentage weight of exams: ";
+                cin >> examWeight;
+                cout << "Enter the percentage weight of projects: ";
+                cin >> projectWeight;
+            } 
         
         // Allow the user to enter multiple scores until the user chooses to stop
         // Based off the assumption that the scores are out of 100 points each.
@@ -93,129 +93,144 @@ int main()
             double tAssignmentWeight, tQuizWeight, tExamWeight, tProjectWeight;
             
         // input loop of homework
-        do {
-            cout << "Enter your assignment score: ";
-            cin >> assignmentScore;
-            totalAssignments += assignmentScore;  // 累加分数
-            amountAssignments += 1;
+            do {
+                cout << "Enter your assignment score: ";
+                cin >> assignmentScore;
+                totalAssignments += assignmentScore;  // 累加分数
+                amountAssignments += 1;
 
-            cout << "Do you have more assignment scores to enter? (y/n): ";
-            cin >> moreInput;
-        } while (moreInput == 'y');
+                cout << "Do you have more assignment scores to enter? (y/n): ";
+                cin >> moreInput;
+            } while (moreInput == 'y');
 
         // input loop of quiz
-        do {
-            cout << "Enter your quiz score: ";
-            cin >> quizScore;
-            totalQuizzes += quizScore;
-            amountQuizzes += 1;
+            do {
+                cout << "Enter your quiz score: ";
+                cin >> quizScore;
+                totalQuizzes += quizScore;
+                amountQuizzes += 1;
 
-            cout << "Do you have more quiz scores to enter? (y/n): ";
-            cin >> moreInput;
-        } while (moreInput == 'y');
+                cout << "Do you have more quiz scores to enter? (y/n): ";
+                cin >> moreInput;
+            } while (moreInput == 'y');
 
         // input loop of exam
-        do {
-            cout << "Enter your exam score: ";
-            cin >> examScore;
-            totalExams += examScore;
-            amountExams += 1;
+            do {
+                cout << "Enter your exam score: ";
+                cin >> examScore;
+                totalExams += examScore;
+                amountExams += 1;
 
-            cout << "Do you have more exam scores to enter? (y/n): ";
-            cin >> moreInput;
-        } while (moreInput == 'y');
+                cout << "Do you have more exam scores to enter? (y/n): ";
+                cin >> moreInput;
+            } while (moreInput == 'y');
 
         // input loop of project
-        do {
-            cout << "Enter your project score: ";
-            cin >> projectScore;
-            totalProjects += projectScore;
-            amountProjects += 1;
+            do {
+                cout << "Enter your project score: ";
+                cin >> projectScore;
+                totalProjects += projectScore;
+                amountProjects += 1;
 
-            cout << "Do you have more project scores to enter? (y/n): ";
-            cin >> moreInput;
-        } while (moreInput == 'y');
+                cout << "Do you have more project scores to enter? (y/n): ";
+                cin >> moreInput;
+            } while (moreInput == 'y');
 
         // input total points
-        cout << "Total assignment points: " << totalAssignments << ", Amount of assignments inputted: " << amountAssignments << endl;
-        cout << "Total quiz points: " << totalQuizzes << ", Amount of quizzes inputted: " << amountQuizzes << endl;
-        cout << "Total exam points: " << totalExams << ", Amount of exams inputted: " << amountExams << endl;
-        cout << "Total project points: " << totalProjects << ", Amount of projects inputted: " << amountProjects << endl;
+            cout << "Total assignment points: " << totalAssignments << ", Amount of assignments inputted: " << amountAssignments << endl;
+            cout << "Total quiz points: " << totalQuizzes << ", Amount of quizzes inputted: " << amountQuizzes << endl;
+            cout << "Total exam points: " << totalExams << ", Amount of exams inputted: " << amountExams << endl;
+            cout << "Total project points: " << totalProjects << ", Amount of projects inputted: " << amountProjects << endl;
 
         // Turn percentage weight of categories into decimals, which will be used for grade calculation.
-        assignmentDecimal = assignmentWeight / 100.0;
-        quizDecimal = quizWeight / 100.0;
-        examDecimal = examWeight / 100.0;
-        projectDecimal = projectWeight / 100.0;
+            assignmentDecimal = assignmentWeight / 100.0;
+            quizDecimal = quizWeight / 100.0;
+            examDecimal = examWeight / 100.0;
+            projectDecimal = projectWeight / 100.0;
 
         // Calculations to find weighted grade (Total points divided by amount of inputs, multiplied by decimal of weight per category)
-        tAssignmentWeight = (totalAssignments / amountAssignments) * assignmentDecimal;
-        tQuizWeight = (totalQuizzes / amountQuizzes) * quizDecimal;
-        tExamWeight = (totalExams / amountExams) * examDecimal;
-        tProjectWeight = (totalProjects / amountProjects) * projectDecimal;
-        double weightedGrade = tAssignmentWeight + tQuizWeight + tExamWeight + tProjectWeight;
-        cout << "Your Weighted Grade is: " << fixed << setprecision(2) << weightedGrade << endl;
+            tAssignmentWeight = (totalAssignments / amountAssignments) * assignmentDecimal;
+            tQuizWeight = (totalQuizzes / amountQuizzes) * quizDecimal;
+            tExamWeight = (totalExams / amountExams) * examDecimal;
+            tProjectWeight = (totalProjects / amountProjects) * projectDecimal;
+            double weightedGrade = tAssignmentWeight + tQuizWeight + tExamWeight + tProjectWeight;
+            cout << "Your Weighted Grade is: " << fixed << setprecision(2) << weightedGrade << endl;
 
-        if (weightedGrade >= GradeA) {
-            cout << "You have an A in this course." << endl;
-        } else if (weightedGrade < GradeA && weightedGrade >= GradeB) {
-            cout << "You have a B in this course." << endl;
-        } else if (weightedGrade < GradeB && weightedGrade >= GradeC) {
-            cout << "You have a C in this course." << endl;
-        } else if (weightedGrade < GradeC && weightedGrade >= GradeD) {
-            cout << "You have a D in this course." << endl;
-        } else if (weightedGrade < GradeD) {
-            cout << "You have a F in this course." << endl;
-        }
+        // Grade Placement after final grade is calculated
+            if (weightedGrade >= GradeA) {
+                cout << "You have an A in this course." << endl;
+            } else if (weightedGrade < GradeA && weightedGrade >= GradeB) {
+                cout << "You have a B in this course." << endl;
+            } else if (weightedGrade < GradeB && weightedGrade >= GradeC) {
+                cout << "You have a C in this course." << endl;
+            } else if (weightedGrade < GradeC && weightedGrade >= GradeD) {
+                cout << "You have a D in this course." << endl;
+            } else if (weightedGrade < GradeD) {
+                cout << "You have a F in this course." << endl;
+            }
 
-    } else if (weighted == "no") {
+        } else if (weightChoice == "n" || weightChoice == "N") {
 
-        cout << "How much points is the class made up of?: ";
-        cin >> TotalPoints;
-        cout << "Minimum Points required to get an A?: ";
-        cin >> GradeA;
-        cout << "Minimum Points required to get a B?: ";
-        cin >> GradeB;
-        cout << "Minimum Points required to get a C?: ";
-        cin >> GradeC;
-        cout << "Minimum Points required to get a D?: ";
-        cin >> GradeD;
+            cout << "How much points is the class made up of?: ";
+            cin >> TotalPoints;
+            cout << "Minimum Points required to get an A?: ";
+            cin >> GradeA;
+            cout << "Minimum Points required to get a B?: ";
+            cin >> GradeB;
+            cout << "Minimum Points required to get a C?: ";
+            cin >> GradeC;
+            cout << "Minimum Points required to get a D?: ";
+            cin >> GradeD;
         
 
         // Since in unweighted grading, all coursework holds the same weight, assignment types will most likely not matter in this portion and will probably be erased.
-        char moreInput;
-        int score, totalScore = 0;
+            char moreInput;
+            int score, totalScore = 0;
 
         // Warning for score inputs
-        cout << "When entering scores, make sure your inputs do not exceed the total points the class is made up of. Doing so will lead to the reset of score inputs.\n";
+            cout << "When entering scores, make sure your inputs do not exceed the total points the class is made up of. Doing so will lead to the reset of score inputs.\n";
 
-        do {
-            cout << "Enter a score: ";
-            cin >> score;
-            totalScore += score;
+            do {
+                cout << "Enter a score: ";
+                cin >> score;
+                totalScore += score;
 
         // Should look into a feature that allows changes in scores to avoid having user input all scores again if a small number error was made
         // As for now, this program essentially makes sure score inputs does not exceed the total score cap.
-            if (totalScore > TotalPoints) {
-                cout << "Your score inputs should not exceed the total amount of points. Please re-enter your scores." << endl;
-                totalScore = 0;
-            }
+                if (totalScore > TotalPoints) {
+                    cout << "Your score inputs should not exceed the total amount of points. Please re-enter your scores." << endl;
+                    totalScore = 0;
+                }
+                cout << "Total Score: " << totalScore << endl;
+                cout << "Do you have more scores to enter? (y/n): ";
+                cin >> moreInput;
+            } while (moreInput == 'y');
 
-            cout << "Total Score: " << totalScore << endl;
-            cout << "Do you have more scores to enter? (y/n): ";
-            cin >> moreInput;
-        } while (moreInput == 'y');
+        // Grade Placement after final grade is calculated
+            if (totalScore >= GradeA) {
+                cout << "You have an A in this course." << endl;
+            } else if (totalScore < GradeA && totalScore >= GradeB) {
+                cout << "You have a B in this course." << endl;
+            } else if (totalScore < GradeB && totalScore >= GradeC) {
+                cout << "You have a C in this course." << endl;
+            } else if (totalScore < GradeC && totalScore >= GradeD) {
+                cout << "You have a D in this course." << endl;
+            } else if (totalScore < GradeD) {
+                cout << "You have a F in this course." << endl;
+            }
 
         // Non-weighted system: Simple point-based calculation
         // Make sure to add code that prevents users from inputting a total score that exceeds the amount user previously mentioned 
         // (ex: if assignments is worth 200 points, user inputs should not exceed 200)
 
-    } else {
-    cout << "This question must be answered properly. Please try again.\n";
-    // Loop back to input for weighted/unweighted question
-    }
-// Later in the code: user output of whether they are doing good, or need to do better in the course
-// Also add code later to see if there are any other courses to test
-// Use functions so we can simplify the structure of the program and be able to use certain parts again
-// Can also potentially add a feature in which we allow the user to input the name of the coursework before asking for score so they do not get confused.
+        // This occurs when there is not a yes or no answer for weighted/unweighted question. Will need to make a loop to ask this question.
+        } else {
+            cout << "Invalid Input. Restart the program.";
+        }
+
+    cout << "Do you want to continue using this program? (y/n): " << endl;
+    cin >> choice;
+
+    } while (choice == "Y" || choice == "y");
+
 }
